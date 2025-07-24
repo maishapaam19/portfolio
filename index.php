b95e5d7156f5dbf37c0e9e2420bda3d8ab3b4fa0
Plonteras191 <plonteras.johnkristoffer@gmail.com>
2025-07-24 19:53:24 +0800

    new features

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="assets/css/header.css" />
+  <link rel="stylesheet" href="assets/css/home.css" />
+  <link rel="stylesheet" href="assets/css/about.css" />
+  <link rel="stylesheet" href="assets/css/skills.css" />
+  <link rel="stylesheet" href="assets/css/project.css" />
+  <link rel="stylesheet" href="assets/css/contact.css" />
+  <link rel="stylesheet" href="assets/css/footer.css" />
+    <title>Document</title>
+</head>
+<body class="home-body">
+
+  <?php include "components/header.html"; ?>
+
+  <main>
+    <?php include "components/home.html"; ?>
+    <?php include "components/about.html"; ?>
+    <?php include "components/skills.html"; ?>
+    <?php include "components/project.html"; ?>
+    <?php include "components/contact.html"; ?>
+  </main>
+
+  <?php include "components/footer.html"; ?>
+
+
+
+</body>
+</html> 
